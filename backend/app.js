c3ba4689f543e15e484f22230ff20d80c6ae1597
khushinj <khushinj0304@gmail.com>
2025-02-21 17:01:21 +0530

    Cors solved

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 app.use(express.urlencoded({ extended: true }));
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 let gfs;
 const secret_key = process.env.secret_key;
 
