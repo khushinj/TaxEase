d3aadefaae7d5986bddca88d5c513af6014143d8
khushinj <khushinj0304@gmail.com>
2025-02-21 16:49:26 +0530

    Cors solved

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(cors({
     origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
 app.use(express.urlencoded({ extended: true }));
